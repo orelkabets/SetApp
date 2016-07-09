8685af4da5f955138e1a7e64b2b0263515eae44e
orelkabets <elkabetsor@gmail.com>
2016-07-09 21:31:23 +0300

    reverting

--- app/src/main/java/com/example/or/setapp/HeaderFragment.java
+++ app/src/main/java/com/example/or/setapp/HeaderFragment.java
@@ -11,7 +11,7 @@ public class HeaderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.header, container, false);
     }
 
