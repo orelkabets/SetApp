68e08bf1e7d3d40ca89c909db3cbef62fcde834c
idan <idank102@gmail.com>
2016-07-15 19:51:38 +0300

    facebook login

--- app/src/main/java/com/example/or/setapp/activities/login.java
+++ app/src/main/java/com/example/or/setapp/activities/login.java
@@ -7,27 +7,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+
 import com.example.or.setapp.R;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class login extends Activity {
 
-    Button facebookLogin,googleLogin;
+    Button googleLogin;
+    LoginButton facebookLogin;
+    CallbackManager callbackManager;
+    String info;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_login);
 
         ActionBar actionBar = getActionBar();
         actionBar.hide();
 
-        facebookLogin = (Button)findViewById(R.id.facebook_login);
+        facebookLogin = (LoginButton)findViewById(R.id.facebook_login);
         googleLogin   = (Button)findViewById(R.id.google_login);
 
 
-        facebookLogin.setOnClickListener(new View.OnClickListener() {
+        facebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
+            public void onSuccess(LoginResult loginResult) {
+                info= "User ID: " + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                        "Auth Token: " + loginResult.getAccessToken().getToken();
+
                 startActivity(new Intent(login.this,loginDetails.class));
+            }
+
+            @Override
+            public void onCancel() {
+                info="Login attempt canceled.";
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info="Login attempt failed.";
             }
         });
         googleLogin.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +66,10 @@ public class login extends Activity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
